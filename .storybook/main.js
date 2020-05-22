cd862cdce2f28d4c71000d5aeb2bd8f6ca1295e0
Ricardo Canelas <ricardocanelas@gmail.com>
2020-05-22 17:09:33 -0300

    feat: include addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],

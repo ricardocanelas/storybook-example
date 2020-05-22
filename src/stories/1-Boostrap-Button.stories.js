3b5eaa2d7ce7374d0ec45393b292c57104768117
Ricardo Canelas <ricardocanelas@gmail.com>
2020-05-22 17:06:37 -0300

    feat: create a story of the react-boostrap/button

---
+++ src/stories/1-Boostrap-Button.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default {
+  title: 'Boostrap-Button',
+  component: Button,
+};
+
+export const Variant = () => {
+  return (
+    <div>
+      <Button variant="default">Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  )
+}

07ed848b0bd139edcf5a0f5604e4277c3c6cfb78
Ricardo Canelas <ricardocanelas@gmail.com>
2020-05-26 16:22:44 -0300

    Updates

---
+++ main.0e58744cbcb0db19f2c0.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{420:function(n,o,p){p(421),p(569),p(1245),p(1247),n.exports=p(1278)},486:function(n,o){}},[[420,1,2]]]);
---
+++ precache-manifest.5cd8ecd540ef0ed8d8c08c7d3a246e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f17fad5cccf3e5eb71528e313f0f97",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d6f26d11ef0e85324ca2.bundle.js"
+  },
+  {
+    "url": "runtime~main.d6f26d11ef0e85324ca2.bundle.js"
+  },
+  {
+    "revision": "a0b34b14180cf31010d9",
+    "url": "static/css/main.07b85a5e.chunk.css"
+  },
+  {
+    "url": "vendors~main.d6f26d11ef0e85324ca2.bundle.js"
+  },
+  {
+    "url": "vendors~main.d6f26d11ef0e85324ca2.bundle.js.LICENSE.txt"
+  }
+]);

59ae953093537139fdd68f084a1294212049778f
Ricardo Canelas <ricardocanelas@gmail.com>
2020-05-27 19:10:22 -0300

    docs: fix Dropdown story

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function App() {
   return (
@@ -19,6 +20,17 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown-Button
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 }
